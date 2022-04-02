41aca581677b9fbe1ab225615e2cc97c035b040a
franklint <42953264+franklint@users.noreply.github.com>
2022-04-02 17:13:24 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Is it alright to send SSN or PII information over email?",
+    answer: "Never",
+    options: [
+      "Only when requested by client",
+      "When requested by my manager",
+      "Never",
+      "Sometimes"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the firms mobile policy?",
+    answer: "Never at the workstation, this is illegal",
+    options: [
+      "During downtime throughout the office day", 
+      "During a bathroom break", 
+      "Never at the workstation, this is illegal",
+      "During the morning only when it's quiet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many QA environments should there be before production?",
+    answer: "2 or more",
+    options: [
+      "None", 
+      "1 or more", 
+      "2 or more", 
+      "Always work in Prod"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the workstation desk policy?",
+    answer: "clean desk policy only",
+    options: [
+      "It's ok to have many papers on the desk", 
+            "some papers is alright", 
+            "clean desk policy only",
+            "It's ok to be messy a few days of the week"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does PII stand for?",
+    answer: "Personal Identifiable Information",
+    options: [
+      "Persons Important Information",
+      "Personal Identifiable Information",
+      "Peoples Information Interpol", 
+      "Paper Information Integrity"
+    ]
+  },
+];
